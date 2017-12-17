bd1967c39a1109f5e230f7e2e995c8cf87bc987b
tkcolorado <cawst222@gmail.com>
2017-12-17 18:42:09 +0900

    Update Adoption.sol

--- pet-shop-tutorial/contracts/Adoption.sol
+++ pet-shop-tutorial/contracts/Adoption.sol
@@ -4,7 +4,7 @@ contract Adoption {
   address[16] public adopters;
   
   // Adopting a pet
-function adopt(uint petId) public returns (uint) {
+  function adopt(uint petId) public returns (uint) {
   require(petId >= 0 && petId <= 15);
 
   adopters[petId] = msg.sender;
@@ -13,7 +13,7 @@ function adopt(uint petId) public returns (uint) {
 }
 
 // Retrieving the adopters
-function getAdopters() public returns (address[16]) {
+  function getAdopters() public returns (address[16]) {
   return adopters;
 }
 
