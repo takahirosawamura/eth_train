3799d68a00b419cb797fd0f113da5d7f570b464f
tkcolorado <cawst222@gmail.com>
2017-12-17 18:40:32 +0900

    Update Adoption.sol

--- pet-shop-tutorial/contracts/Adoption.sol
+++ pet-shop-tutorial/contracts/Adoption.sol
@@ -1,17 +1,21 @@
 pragma solidity ^0.4.4;
-  contract Adoption {
-    address[16] public adopters;
 
-  // Adopting a pet
-  function adopt(uint petId) public returns (uint) {
-    require(petId >= 0 && petId <= 15);
-
-    adopters[petId] = msg.sender;
-    return petId;
-  }
+contract Adoption {
+  address[16] public adopters;
   
-  // Retrieving the adopters
-  function getAdopters() public returns (address[16]) {
-    return adopters;
-  }
+  // Adopting a pet
+function adopt(uint petId) public returns (uint) {
+  require(petId >= 0 && petId <= 15);
+
+  adopters[petId] = msg.sender;
+
+  return petId;
+}
+
+// Retrieving the adopters
+function getAdopters() public returns (address[16]) {
+  return adopters;
+}
+
+
 }
